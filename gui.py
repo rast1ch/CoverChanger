feecd6d194ea27d4208415081ab360ebc38002ed
rast1ch <rast1ch2286@gmail.com>
2020-04-04 22:39:59 +0300

    дфіе

--- gui.py
+++ gui.py
@@ -1,9 +1,53 @@
 import tkinter as tk
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, error
 import tkinter.ttk as ttk
 from tkinter import filedialog
-import image_scraper as i_s
+import bs4 as bs4
+import requests
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, error
+
+def connect(url):
+    global result
+    r = requests.get(url)
+    if str(r.status_code) == '200':
+        soup = bs4.BeautifulSoup(r.content, 'lxml')
+        return soup
+    else:
+        print("Unknown Error")
+        return 0
+
+
+def scrap_to_file(soup):
+    soup = soup.findAll('img')
+    for url in soup:
+        result_img = url['src']
+        result_name = url['alt']
+    result_name = result_name.replace(" ", "_")
+    img = requests.get(result_img)
+    img_file = open(result_name + '.jpg', 'wb')
+    img_file.write(img.content)
+    img_file.close
+    return 1
+
+def image():
+    global x
+    x = filedialog.askopenfilename(initialdir = "/",title = "Select image",filetypes = [("jpeg files","*.jpg")])
+    return x
+
+
+def audio():
+    global y
+    y = filedialog.askopenfilename(initialdir = "/",title = "Select mp3",filetypes = [("mp3 files","*.mp3")])
+    return y
+
+def job(x,y):
+    audio = MP3(y, ID3=ID3)
+    try:
+        audio.add_tags()
+    except error:
+        pass
+    audio.tags.add(APIC(mime='image/jpeg',type=3,desc=u'Cover',data=open(x,'rb').read()))
+    audio.save()
 
 
 root = tk.Tk()
@@ -13,43 +57,15 @@ root.title('music-changer')
 url= tk.Entry(root,  width=300)
 def spotify_get():
     b = url.get()
-    i_s.scrap_to_file(i_s.connect(b))
+    scrap_to_file(connect(b))
 image_download= tk.Button( command=spotify_get,text="image download")
 url.pack()
 image_download.pack()
 
-def image():
-    global x
-    x = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = [("jpeg files","*.jpg")])
-    if x:
-        global label1
-        label1 = ttk.Label(text="Image file loaded successfully").grid(row=1, column=1, padx=4, pady=4, sticky='ew')
+def change():
+    job(image(), audio())
 
-
-def audio():
-    global y
-    y = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = [("mp3 files","*.mp3")])
-    if y:
-        global label2
-        label2 = ttk.Label(text="Audio file loaded successfully").grid(row=2, column=1, padx=4, pady=4, sticky='ew')
-
-
-def job():
-    audio = MP3(y, ID3=ID3)
-    try:
-        audio.add_tags()
-    except error:
-        pass
-    audio.tags.add(APIC(mime='image/jpeg',type=3,desc=u'Cover',data=open(x,'rb').read()))
-    audio.save()
-    global label3
-    label3 = ttk.Label(text="Cover art changed successfully").grid(row=3, column=1, padx=4, pady=4, sticky='ew')
-
-button1 = ttk.Button(root, text="Select Image", command=image)
-button2 = ttk.Button(root, text="Select Audio", command=audio)
-button3 = ttk.Button(root, text="Change", command=job)
-button1.pack()
-button2.pack()
+button3 = ttk.Button(root, text="Change", command=change)
 button3.pack()
 #button4 = ttk.Button(root, text="Clear", command=clear)
 
